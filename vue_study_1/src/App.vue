f17ee5621a1d4a8b2b3a260c695b8de66e653848
jangseohyun <plmet33@naver.com>
2024-09-16 23:56:39 +0900

    [1~4] 환경세팅 및 데이터 바운딩, v-for 반복문 학습

---
+++ vue_study_1/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+
+  <div class="menu">
+    <a v-for="menuNm in menuNmArr" :key="menuNm">{{menuNm}}</a>
+    <!--<a v-for="(menuNm,i) in menuNmArr" :key="i">{{menuNm}}</a>-->
+  </div>
+
+  <img alt="Vue logo" src="./assets/logo.png" />
+
+  <div>
+      <h4 :style="colorRed">{{products[0]}}</h4>
+      <p>{{price1}}만 원</p>
+  </div>
+  <div>
+      <h4>{{products[1]}}</h4>
+      <p>{{price2}}만 원</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  data(){
+    return{
+      price1: 60,
+      price2: 100,
+      products: ['역삼동원룸','천호동원룸','마포구원룸'],
+      colorRed: 'color:red',
+      menuNmArr: ['Home','Products','About']
+    }
+  },
+  components: {
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+</style>
