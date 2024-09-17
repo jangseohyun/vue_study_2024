0553831c5d570dbe5db4474c67251bf166f8d008
jangseohyun <plmet33@naver.com>
2024-09-17 17:32:51 +0900

    [6] v-if와 모달창 제작 학습

--- vue_study_1/src/App.vue
+++ vue_study_1/src/App.vue
@@ -1,12 +1,24 @@
 <template>
 
+  <!--모달창-->
+  <div class="black-bg" v-if="modalOpen==true">
+    <div class="white-bg">
+        <button class="x-btn" @click="modalOpen=false">X</button>
+        <br>
+        <h4>상세페이지</h4>
+        <p>상세 내용</p>
+    </div>
+  </div>
+  <!--모달창-->
+
   <div class="menu">
     <a v-for="menuNm in menuNmArr" :key="menuNm">{{menuNm}}</a>
     <!--<a v-for="(menuNm,i) in menuNmArr" :key="i">{{menuNm}}</a>-->
   </div>
 
   <div v-for="(products,i) in productsArr" :key="i">
-      <h4>{{productsArr[i]}}</h4>
+      <img :src="require(`@/assets/room${i}.jpg`)" class="room-img">
+      <h4 @click="modalOpen==false ? modalOpen=true : modalOpen=false">{{productsArr[i]}}</h4>
       <p>{{priceArr[i]}}만 원</p>
       <button @click="reportNumArr[i]++" @mouseover="logTest">허위매물신고</button> <!-- v-on: → @ -->
       <span>신고수: {{reportNumArr[i]}}</span>
@@ -24,7 +36,8 @@ export default {
       priceArr: [60,100,150],
       productsArr: ['역삼동원룸','천호동원룸','마포구원룸'],
       colorRed: 'color:red',
-      menuNmArr: ['Home','Products','About']
+      menuNmArr: ['Home','Products','About'],
+      modalOpen: false
     }
   },
   methods: {
@@ -56,5 +69,36 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.x-btn {
+  float: right;
 }
 </style>
